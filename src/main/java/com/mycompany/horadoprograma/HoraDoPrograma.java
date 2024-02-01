cd99998291625da7105447ba14e900bc644ea3c9
Cassoli <fernandes.cassoli@gmail.com>
2024-02-01 10:51:07 -0300

    primeiro!

---
+++ src/main/java/com/mycompany/horadoprograma/HoraDoPrograma.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.horadoprograma;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Pessoal
+ */
+public class HoraDoPrograma {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é ");
+        System.out.println(relogio.toString());
+                
+                
+    }
+}
